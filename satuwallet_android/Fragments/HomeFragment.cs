9d760d4138fc7999aa23e2f37be3259563b946f3
nicholasandreasujitno <nicholasandreasujitno@gmail.com>
2019-02-23 10:25:22 +0700

    fix bugs on home fragment

--- satuwallet_android/Fragments/HomeFragment.cs
+++ satuwallet_android/Fragments/HomeFragment.cs
@@ -33,10 +33,34 @@ namespace satuwallet_android.Fragments
             View view = inflater.Inflate(Resource.Layout.frag_home, container, false);
 
 
+            var vwPay = view.FindViewById(Resource.Id.home_vwPay);
+            vwPay.Click += VwPay_Click;
+            var vwWallet = view.FindViewById(Resource.Id.home_vwWallet);
+            vwWallet.Click += VwWallet_Click; ;
+            var vwHistory = view.FindViewById(Resource.Id.home_vwHistory);
+            vwHistory.Click += VwHistory_Click;
+
 
             return view;
             //return base.OnCreateView(inflater, container, savedInstanceState);
         }
-        
+
+        private void VwPay_Click(object sender, EventArgs e)
+        {
+            var i = new Intent(Application.Context, typeof(PayActivity));
+            StartActivity(i);
+        }
+
+        private void VwWallet_Click(object sender, EventArgs e)
+        {
+            var i = new Intent(Application.Context, typeof(WalletActivity));
+            StartActivity(i);
+        }
+
+        private void VwHistory_Click(object sender, EventArgs e)
+        {
+            var i = new Intent(Application.Context, typeof(HistoryActivity));
+            StartActivity(i);
+        }
     }
 }
